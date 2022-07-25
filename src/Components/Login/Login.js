7236409dff57d2c7f40cf1b3157ffc5af371b3c1
khadijatunnasha <khadijatunnashamunny@gmail.com>
2022-07-25 13:57:30 +0600

    login frontend validation and value console done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,10 +2,20 @@ import { Card, Typography,TextField,Grid,Checkbox, Button} from '@mui/material';
 import React from 'react';
 import useStyles from './LoginStyle';
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Login() {
     const classes = useStyles();
+    const {
+      handleSubmit,
+      register,    
+      formState: { errors },
 
+    } = useForm();
+    const onSubmit = (data) => {
+      console.log(data);
+     
+    };
     return (
         <div  style={{
             backgroundColor: "#f7f8fa",
@@ -20,19 +30,16 @@ function Login() {
               Login to Google Classroom and start working
             </Typography>
           </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+
           <Card className={classes.card}>
           <Typography color="primary" variant='h4'>Login</Typography>
           <br/>
           <Grid>
             <Grid item>
-            <div >
-            <TextField style={{ width: "70%", height: "78px" }}
-            className={classes.textField} id="outlined-basic" label="Email*" variant="outlined" />
-            </div>
-            <div>
-            <TextField style={{ width: "70%", height: "78px" }}
-            className={classes.textField} id="outlined-basic" label="Password*" variant="outlined" />
-            </div>
+            <TextField style={{ width: "70%", height: "78px" }} helperText={errors.email?.message} {...register("email", { required: " Email is required" })} error={Boolean(errors.email)} className={classes.textField} id="outlined-basic" name="email" label="Email*" variant="outlined" />
+            <TextField style={{ width: "70%", height: "78px" }} {...register("password", { required: " Password is required" })} type="password" error={Boolean(errors.password)} helperText={errors.password?.message} className={classes.textField} id="outlined-basic" name="password" label="Password*" variant="outlined" />
+           
             <div>
                 <Checkbox
                 color="primary"
@@ -40,7 +47,7 @@ function Login() {
                 Remember Me 
             </div>
             <div>
-            <Button variant="outlined">Login</Button>
+            <Button type="submit" variant="outlined">Login</Button>
             </div>
             <Typography className={classes.title}>
             <strong>Not a member yet?</strong>
@@ -51,6 +58,7 @@ function Login() {
             </Grid>
           </Grid>
           </Card>
+          </form>
        
         </div>
     );
--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -38,17 +38,17 @@ function Registration() {
           <Typography color="primary" variant='h4'>Registration</Typography>
           <br/>
           <Grid>
+              <Grid item>
                   <TextField style={{ width: "70%", height: "78px" }} {...register("name", { required: " Name is required" })} error={Boolean(errors.name)} helperText={errors.name?.message} className={classes.textField} id="outlined-basic" name="name" label="Name*" variant="outlined" />
                   <TextField style={{ width: "70%", height: "78px" }} helperText={errors.email?.message} {...register("email", { required: " Email is required" })} error={Boolean(errors.email)} className={classes.textField} id="outlined-basic" name="email" label="Email*" variant="outlined" />
                   <TextField style={{ width: "70%", height: "78px" }} type="password" {...register("password", { required: " Password is required" })} helperText={errors.password?.message} error={Boolean(errors.password)} className={classes.textField} id="outlined-basic" name="password" label="Password*" variant="outlined" />
                   <TextField style={{ width: "70%", height: "78px" }} type="password" error={Boolean(errors.confirmPassword)}  helperText={errors.confirmPassword?.message}  {...register("confirmPassword", { required: " Confirm Password is required" })}  className={classes.textField} id="outlined-basic" name="confirmPassword" label="Confirm Password*" variant="outlined" />
-               
-                   <div>
-                   <Checkbox
-                   color="primary"
-                   />
-                   Remember Me 
-                   </div>
+                  <div>
+                  <Checkbox
+                  color="primary"
+                  />
+                  Remember Me 
+                  </div>
                 
                   <Button type="submit" variant="outlined">Register</Button>
                   <Typography className={classes.title}>
@@ -57,10 +57,11 @@ function Registration() {
                       <Button  color="primary">Login</Button>
                     </Link>
                   </Typography>
+              </Grid>
             </Grid>
-            </Card>
-          </form>
-       
+          </Card>
+        </form>
+
         </div>
     );
 }
